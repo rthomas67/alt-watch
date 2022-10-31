960b40c085faa8241d614d4973e2958b0dcfcc6c
Ray Thomas (stash) <Ray.Thomas@charter.com>
2022-10-31 17:33:17 -0600

    icon showing up, but not positioned correctly

--- scripts/content.js
+++ scripts/content.js
@@ -1,27 +1,29 @@
-const thumbnails = document.querySelectorAll("a[id='video-title-link']");
+const videoTitleLinks = document.querySelectorAll("a[id='video-title-link']");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
-selectedThumbnails = "";
-thumbnails.forEach((thumbnail) => {
-    selectedThumbnails = selectedThumbnails + thumbnail.textContent;
-//    if (thumbnail) {
-        // const text = article.textContent;
-        // const wordMatchRegExp = /[^\s]+/g; // Regular expression
-        // const words = text.matchAll(wordMatchRegExp);
-        // matchAll returns an iterator, convert to array to get word count
-        // const wordCount = [...words].length;
-        // const readingTime = Math.round(wordCount / 200);
-        // const badge = document.createElement("p");
-        // Use the same styling as the publish information in an article's header
-        // badge.classList.add("color-secondary-text", "type--caption");
-        // badge.textContent = `⏱️ ${readingTime} min read`;
+videoTitleLinks.forEach((videoTitleLink) => {
+    videoTitle = videoTitleLink.textContent;
+    detailsBox = videoTitleLink.closest("div[id='details']");
+    avatarLink = detailsBox.querySelector("a[id='avatar-link']");
+    altWatchDiv = document.createElement("div");
+//    console.log("type of avatarLink: " + typeof(avatarLink) + " -- nodeName: " + avatarLink.nodeName);
+    altWatchButtonLink = document.createElement("img");
+    altWatchButtonLink.src = chrome.runtime.getURL("images/icon-48.png");
+    altWatchDiv.appendChild(altWatchButtonLink);
+    altWatchDiv.height = 48;
+    altWatchDiv.width = 48;
+    altWatchDiv.style.float = "left";
+    // TODO: add onClick() to pop up iframe
+    avatarLink.after(altWatchDiv);
 
-        // Support for API reference docs
-        // const heading = article.querySelector("h1");
-        // Support for article docs with date
-        // const date = article.querySelector("time")?.parentNode;
-        //
-        // (date ?? heading).insertAdjacentElement("afterend", badge);
-//    }
+    // TODO: Search Odysee, BlazeTV, Rumble, etc. etc. to find the identical title
+    //  video (and channel/creator name if similar), grab the URLs, create an
+    //  item in a map for popup content, and create a link/button beside/below
+    //  the videoTitleLink to display an iframe popup with "View on OtherPlatform" links.
+
+    // TODO: Use local storage in the browser to save a list of confirmed
+    //  channel/creator mappings
+    //  e.g. Hickock45 = rumble/real-hickock-45
+    //  e.g. LouderWithCrowder = rumble/LouderCrowderFree
+    //  etc.
 });
-alert(selectedThumbnails);
